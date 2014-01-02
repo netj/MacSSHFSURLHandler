41641b1a7c25ba56e8e3b65957fa32815a12dba0
Jaeho Shin <netj@sparcs.org>
2014-01-02 11:54:23 -0800

    Building the app from scratch with osacompile
    So we can remove .app binaries from the source tree.

--- Build.sh
+++ Build.sh
@@ -1,3 +1,29 @@
 cd "`dirname "$0"`"
-osacompile -o MacSSHFSURLHandler.app/Contents/Resources/Scripts/main.scpt MacSSHFSURLHandler.applescript
+
+echo >&2 "Generating MacSSHFSURLHandler.app"
+osacompile -o MacSSHFSURLHandler.app MacSSHFSURLHandler.applescript
+
+echo >&2 "Bundling sshfs.sh"
 install sshfs.sh MacSSHFSURLHandler.app/Contents/Resources/Scripts/
+
+echo >&2 "Tweaking Info.plist for sshfs: and sftp: URLs"
+{
+    echo 'g/^<key>CFBundleURLTypes/,/^<\/array>/ d'
+    echo '/<key>CFBundleSignature/+1 a'
+    echo '<key>CFBundleURLTypes</key>
+<array>
+	<dict>
+		<key>CFBundleURLName</key>
+		<string>SFTP Filesystem</string>
+		<key>CFBundleURLSchemes</key>
+		<array>
+			<string>sshfs</string>
+			<string>sftp</string>
+		</array>
+	</dict>
+</array>'
+    echo .
+    echo wq
+} | ed MacSSHFSURLHandler.app/Contents/Info.plist >/dev/null
+
+touch MacSSHFSURLHandler.app
